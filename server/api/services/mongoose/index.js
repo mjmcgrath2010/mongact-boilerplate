93adb78ed013eaf3117111d8b76f70b3c2c01591
Mike McGrath <mike@mikemcgrathonline.us>
2018-12-31 21:05:07 -0800

    elquently simple

--- server/api/services/mongoose/index.js
+++ server/api/services/mongoose/index.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 const config = require('../../config');
+const dbHost = config && config.db && config.db.url;
 
 module.exports = (req, res, next) =>
   mongoose
-    .connect(config.db.url)
+    .connect(dbHost)
     .then(next())
     .catch(err => next(err));
