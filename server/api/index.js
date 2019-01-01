87790e4ea645f85f0551696bfc04bbc813b11152
Mike McGrath <mike@mikemcgrathonline.us>
2019-01-01 10:23:33 -0800

    Working

--- server/api/index.js
+++ server/api/index.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
+const db = require('mongoose').connection;
 const database = require('./services/mongoose');
 
 router.use(database);
+
+db.once('open', () => {
+  // Set up API routes
+});
 
 module.exports = router;
