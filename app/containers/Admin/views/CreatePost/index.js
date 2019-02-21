40416f2eb5919fa2717c3980769fb117a9456f02
Mike McGrath <mike@mikemcgrathonline.us>
2019-02-21 17:12:39 -0500

    Title

--- app/containers/Admin/views/CreatePost/index.js
+++ app/containers/Admin/views/CreatePost/index.js
@@ -5,25 +5,48 @@
  */
 
 import React from 'react';
+import styled from 'styled-components';
 import TextEditor from '../../../../components/TextEditor';
+import Input from '../../../../components/ui/Input';
 // import PropTypes from 'prop-types';
-// import styled from 'styled-components';
+
+const HeaderWrapper = styled.div`
+  width: 80%;
+  margin: 1em auto;
+  display: block;
+  position: relative;
+`;
 
 /* eslint-disable react/prefer-stateless-function */
 class CreatePost extends React.Component {
   state = {
-    text: '',
+    content: '',
+    title: '',
   };
 
-  handleChange = value => {
-    this.setState({ text: value });
+  handleEditorChange = value => {
+    this.setState({ content: value });
+  };
+
+  handleChange = name => ({ target }) => {
+    this.setState({ [name]: target.value });
   };
 
   render() {
     return (
       <div>
-        <h1>Create a post</h1>
-        <TextEditor handleChange={this.handleChange} value={this.state.text} />
+        <HeaderWrapper>
+          <Input
+            name="title"
+            label="Title"
+            onChange={this.handleChange('title')}
+            value={this.state.title}
+          />
+        </HeaderWrapper>
+        <TextEditor
+          handleChange={this.handleEditorChange}
+          value={this.state.content}
+        />
       </div>
     );
   }
