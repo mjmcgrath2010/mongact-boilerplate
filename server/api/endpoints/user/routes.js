e7d45dbdabe121ff55e0ef903eb54c3e07f95bdc
Mike McGrath <mike@mikemcgrathonline.us>
2019-02-25 18:38:37 -0500

    Added API Docs

--- server/api/endpoints/user/routes.js
+++ server/api/endpoints/user/routes.js
@@ -10,6 +10,31 @@ router.param('id', controller.params);
 
 router
   .route('/')
+  /**
+   * @api {get} /user/:id Request User information
+   * @apiName GetUser
+   * @apiGroup User
+   *
+   * @apiParam {Number} id Users unique ID.
+   *
+   * @apiSuccess {String} firstname Firstname of the User.
+   * @apiSuccess {String} lastname  Lastname of the User.
+   *
+   * @apiSuccessExample Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *       "firstname": "John",
+   *       "lastname": "Doe"
+   *     }
+   *
+   * @apiError UserNotFound The id of the User was not found.
+   *
+   * @apiErrorExample Error-Response:
+   *     HTTP/1.1 404 Not Found
+   *     {
+   *       "error": "UserNotFound"
+   *     }
+   */
   .get(tokenAuth, isAdmin, controller.get)
   .post(controller.post);
 
