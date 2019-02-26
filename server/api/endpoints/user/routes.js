6a920a3c67ed38d70c1770d4a730662cd7871a88
Mike McGrath <mike@mikemcgrathonline.us>
2019-02-25 19:04:58 -0500

    Api Docs

--- server/api/endpoints/user/routes.js
+++ server/api/endpoints/user/routes.js
@@ -12,19 +12,19 @@ router
   .route('/')
   /**
    * @api {get} /user/:id Request User information
-   * @apiName GetUser
+   * @apiName Get Users
    * @apiGroup User
    *
    * @apiParam {Number} id Users unique ID.
    *
-   * @apiSuccess {String} firstname Firstname of the User.
-   * @apiSuccess {String} lastname  Lastname of the User.
+   * @apiSuccess {String} _id Id of the User.
+   * @apiSuccess {String} username  Username of the User.
    *
    * @apiSuccessExample Success-Response:
    *     HTTP/1.1 200 OK
    *     {
-   *       "firstname": "John",
-   *       "lastname": "Doe"
+   *       "_id": "12345tefwq234dfwavr431rt4g",
+   *       "username": "joe@example.com",
    *     }
    *
    * @apiError UserNotFound The id of the User was not found.
@@ -32,7 +32,7 @@ router
    * @apiErrorExample Error-Response:
    *     HTTP/1.1 404 Not Found
    *     {
-   *       "error": "UserNotFound"
+   *       "error": "User Not Found"
    *     }
    */
   .get(tokenAuth, isAdmin, controller.get)
