cbe4e3328e680310b65bf1595e85a28094f3fc9f
Mike McGrath <mike@mikemcgrathonline.us>
2019-02-25 20:32:13 -0500

    Pushing

--- docs/api_data.js
+++ docs/api_data.js
@@ -2,19 +2,81 @@ define({
   api: [
     {
       type: 'get',
-      url: '/user/:id',
-      title: 'Request User information',
-      name: 'Get_Users',
-      group: 'User',
-      parameter: {
+      url: '/user',
+      title: 'Get Users information',
+      name: 'GetUsers',
+      permission: [
+        {
+          name: 'admin',
+        },
+      ],
+      group: 'Admin',
+      header: {
         fields: {
-          Parameter: [
+          Headers: [
             {
-              group: 'Parameter',
-              type: 'Number',
+              group: 'Headers',
+              type: 'String',
               optional: false,
-              field: 'id',
-              description: '<p>Users unique ID.</p>',
+              field: 'X-Token',
+              description: '<p>API Acess Token.</p>',
+            },
+          ],
+        },
+      },
+      success: {
+        examples: [
+          {
+            title: 'Success-Response:',
+            content:
+              ' HTTP/1.1 200 OK\n[\n {\n   "_id": "12345tefwq234dfwavr431rt4g",\n   "username": "joe@example.com",\n },\n {\n   "_id": "12345tefwq2rewwe34dfwavr431rt4g",\n   "username": "john@example.com",\n }\n ....\n]',
+            type: 'json',
+          },
+        ],
+      },
+      error: {
+        fields: {
+          'Error 4xx': [
+            {
+              group: 'Error 4xx',
+              optional: false,
+              field: 'Error',
+              description: '<p>The user is not an admin.</p>',
+            },
+          ],
+        },
+        examples: [
+          {
+            title: 'Error-Response:',
+            content: 'HTTP/1.1 401 Not Found\n{\n  "error": "User is not Admin."\n}',
+            type: 'json',
+          },
+        ],
+      },
+      version: '0.0.0',
+      filename: 'server/api/endpoints/user/routes.js',
+      groupTitle: 'Admin',
+    },
+    {
+      type: 'get',
+      url: '/user/:id',
+      title: 'Get User information',
+      name: 'GetUser',
+      permission: [
+        {
+          name: 'admin',
+        },
+      ],
+      group: 'User',
+      header: {
+        fields: {
+          Headers: [
+            {
+              group: 'Headers',
+              type: 'String',
+              optional: false,
+              field: 'X-Token',
+              description: '<p>API Acess Token.</p>',
             },
           ],
         },
@@ -52,7 +114,128 @@ define({
             {
               group: 'Error 4xx',
               optional: false,
-              field: 'UserNotFound',
+              field: '404',
+              description: '<p>The id of the User was not found.</p>',
+            },
+          ],
+        },
+        examples: [
+          {
+            title: 'Error-Response:',
+            content: 'HTTP/1.1 404 Not Found\n{\n  "error": "User Not Found"\n}',
+            type: 'json',
+          },
+        ],
+      },
+      version: '0.0.0',
+      filename: 'server/api/endpoints/user/routes.js',
+      groupTitle: 'User',
+    },
+    {
+      type: 'post',
+      url: '/user',
+      title: 'Create a new User',
+      name: 'PostUser',
+      group: 'User',
+      permission: [
+        {
+          name: 'Admin',
+        },
+      ],
+      description:
+        '<p>In this case &quot;apiUse&quot; is defined and used. Define blocks with params that will be used in several functions, so you dont have to rewrite them.</p>',
+      parameter: {
+        fields: {
+          Parameter: [
+            {
+              group: 'Parameter',
+              type: 'String',
+              optional: false,
+              field: 'username',
+              description: '<p>Userame of the User.</p>',
+            },
+            {
+              group: 'Parameter',
+              type: 'String',
+              optional: false,
+              field: 'password',
+              description: '<p>Password of the User.</p>',
+            },
+            {
+              group: 'Parameter',
+              type: 'String',
+              optional: false,
+              field: 'token',
+              description: '<p>Access Token of the User.</p>',
+            },
+          ],
+        },
+      },
+      success: {
+        examples: [
+          {
+            title: 'Success-Response:',
+            content: 'HTTP/1.1 200 OK\n{\n  "_id": "12345tefwq234dfwavr431rt4g",\n  "username": "joe@example.com",\n}',
+            type: 'json',
+          },
+        ],
+      },
+      error: {
+        fields: {
+          'Error 4xx': [
+            {
+              group: 'Error 4xx',
+              optional: false,
+              field: 'Error',
+              description: '<p>The user is not an admin.</p>',
+            },
+          ],
+        },
+        examples: [
+          {
+            title: 'Error-Response:',
+            content: 'HTTP/1.1 401 Not Found\n{\n  "error": "User is not Admin."\n}',
+            type: 'json',
+          },
+        ],
+      },
+      version: '0.0.0',
+      filename: 'server/api/endpoints/user/routes.js',
+      groupTitle: 'User',
+    },
+    {
+      type: 'put',
+      url: '/user/:id',
+      title: 'Change a new User',
+      name: 'PutUser',
+      group: 'User',
+      permission: [
+        {
+          name: 'User',
+        },
+      ],
+      description:
+        '<p>This function has same errors like POST /user, but errors not defined again, they were included with &quot;apiUse&quot;</p>',
+      parameter: {
+        fields: {
+          Parameter: [
+            {
+              group: 'Parameter',
+              type: 'id',
+              optional: false,
+              field: 'id',
+              description: '<p>ID of the User.</p>',
+            },
+          ],
+        },
+      },
+      error: {
+        fields: {
+          'Error 4xx': [
+            {
+              group: 'Error 4xx',
+              optional: false,
+              field: '404',
               description: '<p>The id of the User was not found.</p>',
             },
           ],
--- server/api/endpoints/user/routes.js
+++ server/api/endpoints/user/routes.js
@@ -3,38 +3,10 @@ const router = require('express').Router();
 const controller = require('./controller');
 const { isAdmin, tokenAuth } = require('../utils/auth');
 
-// setup boilerplate route just to satisfy a request
-// for building
-
 router.param('id', controller.params);
 
 router
   .route('/')
-  /**
-   * @api {get} /user/:id Request User information
-   * @apiName Get Users
-   * @apiGroup User
-   *
-   * @apiParam {Number} id Users unique ID.
-   *
-   * @apiSuccess {String} _id Id of the User.
-   * @apiSuccess {String} username  Username of the User.
-   *
-   * @apiSuccessExample Success-Response:
-   *     HTTP/1.1 200 OK
-   *     {
-   *       "_id": "12345tefwq234dfwavr431rt4g",
-   *       "username": "joe@example.com",
-   *     }
-   *
-   * @apiError UserNotFound The id of the User was not found.
-   *
-   * @apiErrorExample Error-Response:
-   *     HTTP/1.1 404 Not Found
-   *     {
-   *       "error": "User Not Found"
-   *     }
-   */
   .get(tokenAuth, isAdmin, controller.get)
   .post(controller.post);
 
@@ -45,3 +17,128 @@ router
   .delete(controller.delete);
 
 module.exports = router;
+
+// //////////////////////////////////////////////
+//               GET REQUEST                   //
+// //////////////////////////////////////////////
+
+/**
+ * @api {get} /user Get Users information
+ * @apiName GetUsers
+ * @apiPermission admin
+ * @apiGroup Admin
+ *
+ * @apiHeader (Headers) {String} X-Token API Acess Token.
+ *
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *    [
+ *     {
+ *       "_id": "12345tefwq234dfwavr431rt4g",
+ *       "username": "joe@example.com",
+ *     },
+ *     {
+ *       "_id": "12345tefwq2rewwe34dfwavr431rt4g",
+ *       "username": "john@example.com",
+ *     }
+ *     ....
+ *    ]
+ *
+ * @apiError Error The user is not an admin.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 401 Not Found
+ *     {
+ *       "error": "User is not Admin."
+ *     }
+ *
+ */
+
+// //////////////////////////////////////////////
+//               POST REQUEST                  //
+// //////////////////////////////////////////////
+
+/**
+ * @api {post} /user Create a new User
+ * @apiName PostUser
+ * @apiGroup User
+ * @apiPermission Admin
+ *
+ * @apiDescription In this case "apiUse" is defined and used.
+ * Define blocks with params that will be used in several functions, so you dont have to rewrite them.
+ *
+ * @apiParam {String} username Userame of the User.
+ * @apiParam {String} password Password of the User.
+ * @apiParam {String} token Access Token of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "_id": "12345tefwq234dfwavr431rt4g",
+ *       "username": "joe@example.com",
+ *     }
+ *
+ * @apiError Error The user is not an admin.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 401 Not Found
+ *     {
+ *       "error": "User is not Admin."
+ *     }
+ *
+ */
+
+// //////////////////////////////////////////////
+//               GET ONE REQUEST               //
+// //////////////////////////////////////////////
+
+/**
+ * @api {get} /user/:id Get User information
+ * @apiName GetUser
+ * @apiPermission admin
+ * @apiGroup User
+ *
+ * @apiHeader (Headers) {String} X-Token API Acess Token.
+ *
+ * @apiSuccess {String} _id Id of the User.
+ * @apiSuccess {String} username  Username of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "_id": "12345tefwq234dfwavr431rt4g",
+ *       "username": "joe@example.com",
+ *     }
+ *
+ * @apiError 404 The id of the User was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "User Not Found"
+ *     }
+ */
+
+// //////////////////////////////////////////////
+//               PUT REQUEST                   //
+// //////////////////////////////////////////////
+
+/**
+ * @api {put} /user/:id Change a new User
+ * @apiName PutUser
+ * @apiGroup User
+ * @apiPermission User
+ *
+ * @apiDescription This function has same errors like POST /user, but errors not defined again, they were included with "apiUse"
+ *
+ * @apiParam {id} id ID of the User.
+ *
+ * @apiError 404 The id of the User was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "User Not Found"
+ *     }
+ */
